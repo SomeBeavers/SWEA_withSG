477974f9006e8b1e8a28afcc53d6179a91b9cc4b
Lilia Shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2021-05-26 14:00:27 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            TestNS.TestCls.TestMethod();
+        }
+
+        string? M() => null;
+    }
+}
+
+class NoError { }
+class Warn { }
+
---
+++ SourceGen/TestDataGenerator.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SourceGen
+{
+    [Generator]
+    public class TestDataGenerator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            // Commenting out this line make it resolve
+            context.RegisterForSyntaxNotifications(() => new Pizza());
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+          var noError = context.Compilation.GetTypeByMetadataName("NoError");
+          var warn = context.Compilation.GetTypeByMetadataName("Warn");
+          if (noError == null)
+          {
+            context.AddSource(
+              "Pizza.cs",
+              "namespace TestNS{public class TestCls{public static string TestMethod(){System.Console.WriteLine(\"Generator\");}}}"
+            );
+          }
+          else if (warn != null)
+          {
+            context.AddSource(
+              "Pizza.cs",
+              "namespace TestNS{public class TestCls{private int unused;private string? unused2;public static void TestMethod(){System.Console.WriteLine(\"Generator\");}}}"
+            );
+          }
+          else
+          {
+            context.AddSource(
+              "Pizza.cs",
+              "namespace TestNS{public class TestCls{public static void TestMethod(){System.Console.WriteLine(\"Generator\");}}}"
+            );
+          }
+        }
+    }
+
+    public class Pizza : ISyntaxContextReceiver
+    {
+        public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
+        {
+        }
+    }
+}
